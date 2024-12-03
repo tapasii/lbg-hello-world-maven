be0f3464fbf7090acb84657918b42c78b7229d4c
tapasi <tapasiadhikariworkemail@gmail.com>
2024-12-03 14:04:11 +0000

    Edited: Added a line to say Hello Sharief

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Tapasi"));
+        System.out.println(sayHelloToSomeone("Sharief"));
         System.out.println(sayGoodbye());
     }
 
